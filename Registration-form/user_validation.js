e4b3c56e6fe26dbc2c2ddb0bddb41e42f380beb9
Supimi Piumika <supimi.15@cse.mrt.ac.lk>
2017-11-08 17:22:36 +0530

    user_registration_process_added

---
+++ Registration-form/user_validation.js
@@ -0,0 +1,43 @@
+
+var check = function() {
+        if (document.getElementById('password').value ==
+            document.getElementById('confirm_password').value ) {
+            document.getElementById('message').innerHTML = '';
+            document.getElementById('message1').innerHTML ='';
+            if(check_length(6)){
+                document.getElementById("submit_btn").disabled = false;
+            }
+            else{
+                document.getElementById('message1').style.color = 'red';
+                document.getElementById('message1').innerHTML = 'Password should contain minimum 6 characters'
+                document.getElementById("submit_btn").disabled = true;
+            }
+
+        } else {
+            document.getElementById('message').style.color = 'red';
+            document.getElementById('message').innerHTML = 'Not matching with Password';
+            document.getElementById("submit_btn").disabled = true;
+        }
+    }
+
+var check_length=function(len){
+    if(document.getElementById('password').value.length>=len){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+var check_tel =function () {
+    if ((/^\d+$/.test(document.getElementById(contact_number1).value))&& check_length(9)){
+        document.getElementById('message').innerHTML = '';
+    }
+    else
+    {
+        document.getElementById('message2').style.color = 'red';
+        document.getElementById('message2').innerHTML = 'Enter Valid Contact Numbers'
+
+    }
+}
+
--- inc/register.php
+++ inc/register.php
@@ -6,21 +6,91 @@
  * Time: 9:17 PM
  */
 
-function register(){
+function register($array){
     include '../connection.php';
     $con = connect();
+    $query="select user_id from user_details where user_id='";
+    $query.=$array['index_number'];
+    $query.="'";
+    $dis=mysqli_query($con,$query);
+    if(($dis->num_rows)==0){
+        $query1="insert into user_details (user_id, first_name, last_name,gender,street,town, city,current_year) values ('";
+        $query1 .= $array['index_number'];
+        $query1 .= "','";
+        $query1 .= $array['first_name'];
+        $query1 .= "','";
+        $query1 .= $array['last_name'];
+        $query1 .= "','";
+        $query1 .= $array['gender'];
+        $query1 .= "','";
+        $query1 .= $array['street'];
+        $query1 .="','";
+        $query1 .=$array['town'];
+        $query1 .= "','";
+        $query1 .= $array['city'];
+        $query1 .= "','";
+        $query1 .= $array['accomodation_year'];
+        $query1 .= "')";
+
+
+
+        $query2="insert into login_details (id, pwd, member_type,email) values ('";
+        $query2 .= $array['index_number'];
+        $query2 .= "','";
+        $query2 .= md5($array['password']);
+        $query2 .= "','Student','";
+        $query2 .= $array['email'];
+        $query2 .= "')";
+
+        $query3="insert into student (user_id, dept_name, faculty,academic_year) values ('";
+        $query3 .= $array['index_number'];
+        $query3 .= "','";
+        $query3 .= $array['department'];
+        $query3 .= "','";
+        $query3 .= $array['faculty'];
+        $query3 .="','";
+        $query3 .=$array['academic_year'];
+        $query3 .= "')";
+
+
+        $query4="insert into user_telephone_number (user_id, telephone_num) values ('";
+        $query4 .= $array['index_number'];
+        $query4 .= "','";
+        $query4 .= $array['contact_number1'];
+        $query4 .= "')";
+        if(isset($array['contact_number2'])){
+            $query4 .=",('";
+            $query4 .= $array['index_number'];
+            $query4 .= "','";
+            $query4 .=$array['contact_number2'];
+            $query4 .= "')";
+        }
+
+        $added1 =mysqli_query($con,$query1);
+        $added2 =mysqli_query($con,$query2);
+        $added3 =mysqli_query($con,$query3);
+        $added3 =mysqli_query($con,$query4);
+
+        mysqli_close($con);
+        header("Location:../Admin/index.html");
+        exit();
+
+
+
+    }
+    else {
+        mysqli_close($con);
+        echo "<script language='javascript'>";
+        echo "alert('You are already registered')";
+        echo "</script>";
+        header("Location:../Log-in-form/index.html");
+        exit();
+
+    }
+
 
 }
-echo "hello";
-
-echo var_dump($_POST);
-
-echo print_r($_POST);
-
-foreach ($_POST as $key=>$value){
-    echo $key;
-    echo $value;
-
-}
+register($_POST);
 
 ?>
+
